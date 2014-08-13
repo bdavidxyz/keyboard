672f661ff40a8a5a6da25d0f2176ff0ec9b95ee4
davidb583 <fishing_junk@yahoo.fr>
2014-08-13 15:43:07 +0200

    adding exercice8

---
+++ exercice8/javascript/javascript-walkthrough.js
@@ -0,0 +1,91 @@
+/*
+ * object creation
+ * FROM
+ *
+ * TO
+ * var anything = ;
+ */
+
+
+/*
+ * object creation
+ * FROM
+ *
+ * TO
+ * var o = {};
+ */
+
+
+/*
+ * object creation
+ * FROM
+ *
+ * TO
+ * var r = / /;
+ */
+
+
+/*
+ * object creation
+ * FROM
+ *
+ * TO var s = "";
+ */
+
+
+/*
+ * field addition
+ * FROM
+ * var o = {};
+ * TO
+ * o = {
+ *  key:"value",
+ * }
+ */
+var o = {};
+
+/*
+ * comma automated addition
+ * FROM
+ * useful comma
+ * TO
+ * useful comma,
+ */
+useful comma
+
+/*
+ * comma automated deletion
+ * FROM
+ * unuseful comma,
+ * TO
+ * unuseful comma
+ */
+unuseful comma,
+
+/*
+ * semicolon automated addition
+ * FROM
+ * missing semicolon
+ * TO
+ * missing semicolon;
+ */
+missing semicolon
+
+/*
+ * automagic () addition
+ * FROM
+ * magic
+ * TO
+ * (magic)
+ */
+magic
+
+/*
+ * automagic go to line under
+ * FROM
+ * textCURSORHEREtext
+ * TO
+ * texttext
+ * CURSORHERE
+ */
+ texttext
